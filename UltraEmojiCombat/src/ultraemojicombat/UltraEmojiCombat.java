a2ce4c821bbf231d273b1d5ede3076fc3b4fb094
pfilho1 <111535487+pfilho1@users.noreply.github.com>
2023-05-27 17:54:43 -0300

    Add files via upload

---
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
@@ -0,0 +1,27 @@
+package ultraemojicombat;
+
+public class UltraEmojiCombat {
+
+    public static void main(String[] args) {
+        Lutador l[] = new Lutador[6];
+        l[0] = new Lutador("Pretty Boy", "Franca", 31, 1.75f, 68.9f, 11, 2, 1);
+        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        l[2] = new Lutador ("Snapshadow", "EUA", 35, 1.65f, 80.91f, 12, 2, 1);
+        l[3] = new Lutador ("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
+        l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f,119.3f, 5, 4, 3);
+        l[5] = new Lutador ("Nerdaart", "EUA", 30, 1.81f,105.7f, 12, 2, 4);
+        
+        Luta UEC01 = new Luta();
+        System.out.println("");
+        UEC01.marcarLuta(l[0], l[1]);
+        System.out.println("");
+        UEC01.lutar();
+        System.out.println("");
+        
+        l[0].status();
+        System.out.println("");
+        l[1].status();
+
+    }
+    
+}
